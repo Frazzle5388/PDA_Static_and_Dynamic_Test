b232622c8a72d0d86b776701d65e06195482723c
Fraser Dillon <fraserdillon050388@gmail.com>
2022-03-15 17:20:06 +0000

    Tests updated to call methods and 'Tests PASS' screenshot updated

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -3,18 +3,19 @@ from src.card import Card
 from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
-
+    
     def setUp(self):
         self.card1 = Card("Spades", 1)
         self.card2 = Card("Hearts", 5)
         self.card3 = Card("Diamonds", 8)
+        self.cards = [self.card1, self.card2, self.card3]
+        self.cardgame = CardGame 
 
-    def test_if_card_value_is_one(self):
-        self.assertEqual(True, self.card1.value == 1)
-
-    def test_which_card_has_higher_value(self):
-        self.assertEqual(1 < 5, self.card1.value < self.card2.value)
-
-    def test_total_card_value(self):
-        self.assertEqual(14, self.card1.value + self.card2.value + self.card3.value) 
+    def test_if_player_has_ace(self):
+        self.assertEqual(True, self.cardgame.check_for_ace(self, self.card1))
     
+    def test_which_card_has_higher_value(self):
+        self.assertEqual(self.card3, self.cardgame.highest_card(self, self.card1, self.card3))
+    
+    def test_total_card_value(self):
+        self.assertEqual("You have a total of 14", self.cardgame.cards_total(self, self.cards))
